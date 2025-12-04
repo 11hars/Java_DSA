be5d7ad6d092f46e912241c302a832fb5581f7a5
Harshini T <159106243+11hars@users.noreply.github.com>
2025-12-04 11:08:54 +0530

    Add files via upload

---
+++ CountdigitOccurance.java
@@ -0,0 +1,14 @@
+// Counting Digits of a Number
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in); 
+        int n = obj.nextInt();
+        int count = 0;
+        while(n != 0) {
+            n = n / 10; 
+            count++;   
+        }
+        System.out.println(count);
+    }
+}
